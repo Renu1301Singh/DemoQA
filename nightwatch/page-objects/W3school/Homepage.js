c5097dc818e8fda86f8ce9ca59eafcea0ce65995
Renu1301Singh <Renu.Singh@knoldus.com>
2024-03-04 17:55:49 +0530

    final commit

--- nightwatch/page-objects/W3school/Homepage.js
+++ nightwatch/page-objects/W3school/Homepage.js
@@ -17,54 +17,68 @@ module.exports = {
         selectMonth: '.react-datepicker__month-select>[value="2"]',
         selectdate: ".react-datepicker__day.react-datepicker__day--022",
         selectYear:'.react-datepicker__year-select>[value="2024"]',
-        
+
         subjects:'#subjectsInput',
         selectSubject: '.subjects-auto-complete__menu.css-2613qy-menu ',
         Hobbies:`label[for='hobbies-checkbox-1']`,
         currentAddress: '#currentAddress',
+
         selectStateDropDown: 'div:nth-child(2)>div>div>div>.css-tlfecz-indicatorContainer', 
+        inputState:'#react-select-3-input',
+        inputCity:'#react-select-4-input',
+        
         SelectCityDropDown:'div:nth-child(3)>div>div>div>.css-tlfecz-indicatorContainer',       
         submitButton:'.btn.btn-primary',
         SubmitMessage: '#example-modal-sizes-title-lg',
-        closeButton:'#closeLargeModal',
+        closeButton:'.modal-footer>button',
         praticeFormTitle:'.text-center',
 
     },
     commands: [ {
         enterFirstName: function(value){
             return this
+            .waitForElementVisible('@firstNameInput')
             .setValue('@firstNameInput', value);        
         },
         enterLastName: function(value){
             return this
+            .waitForElementVisible('@lastName')
             .setValue('@lastName',value);
           
         },
         enterEmail: function(value){
             return this
+            .waitForElementVisible('@email')
             .setValue('@email',value)
         },
         selectGender: function(){
             return this
             .assert.visible('@Gender')
+            .waitForElementVisible('@Gender')
             .click('@Gender')
         },
         enterMobile: function(value){
             return this
+            .waitForElementVisible('@Mobile')
             .setValue('@Mobile',value)
         },
 
         enterDateOfBirth: function(){
             return this
+            .waitForElementVisible('@dateOfBirth')
             .click('@dateOfBirth')
+            .waitForElementVisible('@selectMonth')
             .click('@selectMonth')
+            .waitForElementVisible('@selectYear')
             .click('@selectYear')
+            .waitForElementVisible('@selectdate')
             .click('@selectdate')   
         },
 
         inputSubject: function(value){
             return this
             .assert.visible('@subjects')
+            .waitForElementVisible('@subjects')
             .setValue('@subjects',value)
 
         },
@@ -73,40 +87,52 @@ module.exports = {
         selectHobbies: function(){
             return this
             .assert.visible('@Hobbies') 
+            .waitForElementVisible('@Hobbies')
             .click('@Hobbies');
           
         },
         enterCurrentAddress: function(value){
             return this
+            .waitForElementVisible('@currentAddress')
             .setValue('@currentAddress',value);
           
         },
         
-        clickDropDownState: function() {
+        clickDropDownState: function(value) {
             return this
               .assert.visible('@selectStateDropDown') 
-              .click(['@selectStateDropDown',browser.Keys.ARROW_DOWN, browser.Keys.TAB]) 
+              .waitForElementVisible('@selectStateDropDown')
+              .click('@selectStateDropDown',) 
+              .waitForElementVisible('@inputState')
+              .setValue('@inputState',value);
             
           },
     
-        clickDropDownCity: function(){
+        clickDropDownCity: function(value){
             return this
             .assert.visible('@SelectCityDropDown')
+            .waitForElementVisible('@SelectCityDropDown')
             .click('@SelectCityDropDown')
+            .waitForElementVisible('@inputCity')
+            .setValue('@inputCity',value);
                      
          
       },
     submitButton(){
-        return this.click('@submitButton')
+        return this
+        .waitForElementVisible('@submitButton')
+        .click('@submitButton')
     },
 
       submissionMessage(){
         return this
+        .waitForElementVisible('@SubmitMessage')
         .assert.textContains('@SubmitMessage',"Thanks for submitting the form")
       },
 
       closeForm(){
         return this
+        .waitForElementVisible('@closeButton')
         .click('@closeButton')
       },
     }],
--- test/Test.js
+++ test/Test.js
@@ -15,25 +15,25 @@ describe('Filling Pratice Form of DemoQA Website for checking mandatory input fi
     it('1.) User entering valid input in First Name  : ', function(){
       homePage.expect.element('@firstNameInput').visible      
       homePage.enterFirstName('Demo') })
-      homePage.pause(1000)
+      
      
     it('2.) User entering valid input in Last Name : ', function(){
         homePage.expect.element('@lastName').visible
         homePage.enterLastName('Testing')
-        homePage.pause(1000)
+       
       
     })
 
     it('3.) User entering valid input in Email : ', function(){
       homePage.expect.element('@email').visible
       homePage.enterEmail('Demo@gmail.com')
-      homePage.pause(1000)
+     
       
     })
 
     it('4.) User entering valid input in Email : ', function(){
       homePage.selectGender()
-      homePage.pause(3000)
+     
       homePage.assert.elementPresent('#gender-radio-2:checked')
       
     })
@@ -41,99 +41,134 @@ describe('Filling Pratice Form of DemoQA Website for checking mandatory input fi
     it('5.) User entering valid input in Email : ', function(){
       homePage.expect.element('@Mobile').visible
       homePage.enterMobile('9988998877')
-      homePage.pause(1000)
+    
     })
 
     it('6.) User is able to select date of current year : ', function(){
       homePage.expect.element('#dateOfBirthInput').visible
       homePage.enterDateOfBirth()
-      homePage.pause(1000)
+    
     })
+    
 
-      it('7.) User is able to enter atleast one subject : ', function(){
-     
-      homePage.inputSubject(['Maths', browser.Keys.TAB])  
-      homePage.pause(2000)
-    })
 
-      it('8.) User is able to select atleast one of the hobbies: ', function(){
+      it('8.) User is able to select a hobbies: ', function(){
       homePage.expect.element('@Hobbies').visible
+
       homePage.selectHobbies()
-      homePage.pause(2000)
+      
       homePage.assert.elementPresent('#hobbies-checkbox-1:checked');
-      homePage.pause(2000)
+      
     
     })
 
       it('9.) User is able to enter address using alphabets and numbers : ', function(){
       homePage.enterCurrentAddress('Demo Testing Website, 101')
-      homePage.pause(2000)
+     
       })
 
-     // .clickDropDownState()
-      //homePage.pause(1000)
-  
-    //  .clickDropDownCity()
-    //  homePage.pause(1000)
-    // //  .browser.Keys.ARROW_DOWN, browser.Keys.TAB  
-    // // //  homePage.pause(1000)
+      it('10.) User should be able to select a state : ', function(){
 
-    it('10.) User should be able to click on submit button with entering required fields : ', function(){
+      homePage.clickDropDownState('uttar')
+      homePage.pause(1000)
+      homePage.waitForElementVisible('.css-11unzgr',5000,function()
+      {
+        homePage.click('#react-select-3-option-1')
+      
+      })
+
+      })
+
+      it('10.) User should be able to select city : ', function(){
+
+        homePage.clickDropDownCity('agra')
+       
+        homePage.waitForElementVisible('.css-11unzgr',5000,function()
+        {
+          homePage.click('#react-select-4-option-0')
+        
+        })
+        })
+        it('7.) User is able to select a subject : ', function(){
+
+          homePage.inputSubject('Ma')
+        
+          
+          homePage.waitForElementVisible('.subjects-auto-complete__option.subjects-auto-complete__option--is-focused.css-1n7v3ny-option',5000,function()
+          {
+            homePage.click('#react-select-2-option-0')
+    
+          })
+         
+        })
+       
+
+    it('12.) User should be able to click on submit button with entering required fields : ', function(){
       homePage.waitForElementVisible('@submitButton')
-      homePage.pause(2000)
+     
       homePage.submitButton() 
+   
+      
     })
 
-    it('11.) User should be able to see Submission Message : ', function(){
+    it('13.) User should be able to see Submission Message : ', function(){
       homePage.submissionMessage()
-      homePage.pause(2000)
+     
       homePage.expect.element('@SubmitMessage').text.to.contain('Thanks for submitting the form')
 
     })
-    it('12.) User is able to see all added enteries : ', function(){
+    it('14.) User is able to see all added enteries : ', function(){
 
       homePage.expect.element('tbody tr:nth-child(1) td:nth-child(2)').text.to.contain('Demo Testing')
-      homePage.pause(1000)
+     
       homePage.expect.element('tbody tr:nth-child(3) td:nth-child(2)').text.to.contain('Female')
-      homePage.pause(1000)
+    
       homePage.expect.element('tbody tr:nth-child(4) td:nth-child(2)').text.to.contain('9988998877')
-      homePage.pause(1000)
+     
       homePage.expect.element('tbody tr:nth-child(5) td:nth-child(2)').text.to.contain('22 March,2024')
-      homePage.pause(1000)
+    
+      homePage.expect.element('tbody tr:nth-child(6) td:nth-child(2)').text.to.contain('Maths')
+     
       homePage.expect.element('tbody tr:nth-child(7) td:nth-child(2)').text.to.contain('Sports')
-      homePage.pause(1000)
+   
       homePage.expect.element('tbody tr:nth-child(9) td:nth-child(2)').text.to.contain('Demo Testing Website, 101')
-      homePage.pause(1000)
+    
+      homePage.expect.element('tbody tr:nth-child(10) td:nth-child(2)').text.to.contain('Uttar Pradesh Agra')
+      
+      
 
     })
 
-    it('13.) user is able to close the filled form display: ', function(){
-      homePage.closeForm;
-      homePage.pause(1000)
-      homePage.expect.element('@praticeFormTitle').text.to.contain('Practice Form')
+    it('15.) user is able to close the filled form display: ', function(){
+      homePage.waitForElementVisible('@closeButton')
+      homePage.click('@closeButton');
+     
+      
     })
 
-    it('14.) Entering Inavlid Email Should show error: ', function(){
+    it('16.) Entering Inavlid Email will not submit form : ', function(){
       homePage.expect.element('@firstNameInput').visible      
       homePage.enterFirstName('Demo') 
       homePage.expect.element('@lastName').visible
       homePage.enterLastName('Testing')
       homePage.expect.element('@email').visible
       homePage.enterEmail('Demo@gmail')
-      homePage.pause(1000)
+    
       homePage.selectGender()
-      homePage.pause(3000)
+    
       homePage.expect.element('@Mobile').visible
       homePage.enterMobile('9988998877')
       homePage.expect.element('#dateOfBirthInput').visible
       homePage.enterDateOfBirth()
       homePage.expect.element('@Hobbies').visible
       homePage.selectHobbies()
-      homePage.pause(2000)
+     
       homePage.enterCurrentAddress('Demo Testing Website, 101')
       homePage.waitForElementVisible('@submitButton')
       homePage.submitButton() 
-      homePage.assert.visible('@practiceFormTitle');
+   
+      homePage.expect.element('.text-center').text.to.contain('Practice Form')
+    
 
 
     })
